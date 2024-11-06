03fc0de4acadafa264c319159953cd05a1fcee97
Karolina Surma <ksurma@redhat.com>
2024-11-06 12:31:33 +0100

    Add centpkg to use in CentOS Stream
    If tests are run in this repository, they exist.
    Other packages in Fedora require fedpkg-minimal to download the sources.
    We also want to run this test in CentOS Stream where fedpkg-minimal is
    not available, hence adding centpkg invocation.
    The change will have no effect in this repository, but is essential for
    the smooth tests run of other components.

--- tests/download_data_and_run_pytest.sh
+++ tests/download_data_and_run_pytest.sh
@@ -4,10 +4,11 @@
 # When the tests run in python-rpm-generators,
 # the structure on disk does not match the dist-git repository.
 # We apparently must use the standard-test-source role to grab the sources.
-# OTOH in other packages, we must use fedpkg(-minimal).
-# The --force flag is required in full-blown fedpkg (the source is unused in spec),
-# and it is ignored in fedpkg-minimal (all sources are always downloaded).
-test -f test-sources-*.tar.gz || fedpkg sources --force
+# OTOH in other packages, we must use fedpkg(-minimal) or centpkg(-minimal),
+# depending on the destination OS.
+# The --force flag is required in full-blown fedpkg/centpkg (the source is unused in spec),
+# and it is ignored in fedpkg/centpkg-minimal (all sources are always downloaded).
+test -f test-sources-*.tar.gz || fedpkg sources --force || centpkg sources --force
 
 tar -xvf test-sources-*.tar.gz -C ./tests/data/scripts_pythondistdeps/
 cd tests/
